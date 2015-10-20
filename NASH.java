83db4424bca02d50ff25301b97c6600dc0b1b913
RampageNIk <nikkitagangsta@gmail.com>
2015-10-20 19:29:36 +0300

    Rename README.md to NASH.java

---
+++ NASH.java
@@ -0,0 +1,7 @@
+# UnstoppableTeam
+echo # UnstoppableTeam >>NASH.java
+git init
+git add NASH.java
+git commit -m "first commit"
+git remote add origin https://github.com/UnstopableTeam/UnstoppableTeam.git
+git push -u origin master
