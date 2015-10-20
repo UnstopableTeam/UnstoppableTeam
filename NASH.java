42bb37a7fdc4ab9c9ff4313ac4b3af3e715906cd
RampageNIk <nikkitagangsta@gmail.com>
2015-10-20 19:33:15 +0300

    Update NASH.java

--- NASH.java
+++ NASH.java
@@ -6,3 +6,4 @@ git commit -m "first commit"
 git remote add origin https://github.com/UnstopableTeam/UnstoppableTeam.git
 git push -u origin master
 $ git commit -m 'Комментарий к коммиту'
+$ git checkout -b [name_of_your_new_branch]
